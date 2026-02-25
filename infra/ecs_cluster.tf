9bf7a8be60af3e46aa475fd98d16d11392086dde
abhishek095859 <abhishek.narmula@minfytech.com>
2026-02-25 12:50:55 +0530

    Update ECS cluster configuration for Mumbai region

--- infra/ecs_cluster.tf
+++ infra/ecs_cluster.tf
@@ -1,19 +1,30 @@
-# 1. The Cluster
+# 1. Fetch the latest ECS-Optimized AMI for Mumbai (ap-south-1)
+data "aws_ssm_parameter" "ecs_ami" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2023/recommended/image_id"
+}
+
+# 2. The Cluster
 resource "aws_ecs_cluster" "main" {
   name = "my-app-cluster"
 }
 
-# 2. IAM Role for the EC2 Instances to join ECS
+# 3. IAM Role for the EC2 Instances to join ECS
 resource "aws_iam_role" "ecs_node_role" {
   name = "ecs-node-role"
   assume_role_policy = jsonencode({
-    Version = "2012-10-17", Statement = [{ Action = "sts:AssumeRole", Effect = "Allow", Principal = { Service = "ec2.amazonaws.com" } }]
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+    }]
   })
 }
 
+# FIXED: Added the double colon (::) to the policy_arn
 resource "aws_iam_role_policy_attachment" "ecs_node_policy" {
   role       = aws_iam_role.ecs_node_role.name
-  policy_arn = "arn:aws:iam:aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
 resource "aws_iam_instance_profile" "ecs_node_profile" {
@@ -21,17 +32,27 @@ resource "aws_iam_instance_profile" "ecs_node_profile" {
   role = aws_iam_role.ecs_node_role.name
 }
 
-# 3. Launch Template (What kind of EC2?)
+# 4. Launch Template
 resource "aws_launch_template" "ecs_lt" {
   name_prefix   = "ecs-template"
-  image_id      = "ami-0c101f26f147fa7fd" # Amazon Linux 2023 ECS-Optimized
+  image_id      = data.aws_ssm_parameter.ecs_ami.value # Uses the dynamic ID for Mumbai
   instance_type = "t3.micro"
-  iam_instance_profile { name = aws_iam_instance_profile.ecs_node_profile.name }
+  
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ecs_node_profile.name
+  }
+
   vpc_security_group_ids = [aws_security_group.ecs_sg.id]
-  user_data = base64encode("#!/bin/bash\necho ECS_CLUSTER=${aws_ecs_cluster.main.name} >> /etc/ecs/ecs.config")
+
+  # This joins the EC2 instance to your ECS cluster at boot
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    echo ECS_CLUSTER=${aws_ecs_cluster.main.name} >> /etc/ecs/ecs.config
+  EOF
+  )
 }
 
-# 4. Auto Scaling Group
+# 5. Auto Scaling Group
 resource "aws_autoscaling_group" "ecs_asg" {
   vpc_zone_identifier = [aws_subnet.pub.id]
   desired_capacity    = 1
@@ -45,7 +66,7 @@ resource "aws_autoscaling_group" "ecs_asg" {
 
   tag {
     key                 = "AmazonECSManaged"
-    value               = "true" # Note: ASG tag values must be strings
+    value               = "true"
     propagate_at_launch = true
   }
 }
