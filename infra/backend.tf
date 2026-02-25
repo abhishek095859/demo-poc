d4cdc8ca38706e7cfd1f29d1fd5243127f846dc4
abhishek095859 <abhishek.narmula@minfytech.com>
2026-02-25 12:43:06 +0530

    Change S3 backend region to ap-south-1

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "my-ecs-tfstate-bucket" # Change this to your bucket name
     key    = "ecs/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
