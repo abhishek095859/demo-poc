af8ee9cd04d22445eda624954123fc4a520f7c45
abhishek095859 <abhishek.narmula@minfytech.com>
2026-02-25 14:48:15 +0530

    Update vpc.tf

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -34,6 +34,12 @@ resource "aws_security_group" "ecs_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 3000
+    to_port     = 5000 
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
