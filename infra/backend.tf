12e6235a07ce70e844425d9497c9c78e51d6d246
abhishek095859 <abhishek.narmula@minfytech.com>
2026-02-25 13:53:39 +0530

    Update S3 key for Terraform state file

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-ecs-tfstate-bucket" # Change this to your bucket name
-    key    = "ecs/terraform.tfstate"
+    key    = "ecs/v1/terraform.tfstate"
     region = "ap-south-1"
   }
 }
